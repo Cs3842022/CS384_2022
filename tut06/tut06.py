1658bd02dd5c57b94535066351e554c1c744bf71
Cs3842022 <110086201+Cs3842022@users.noreply.github.com>
2022-09-22 17:11:20 +0530

    Assignment 6 - Attendance Report Generator

---
+++ tut06/tut06.py
@@ -0,0 +1,14 @@
+
+def attendance_report():
+###Code
+
+from platform import python_version
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+
+attendance_report()
