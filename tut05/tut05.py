ab624c635179e979b5f87fabbbbfd2b03c792e2a
Cs3842022 <110086201+Cs3842022@users.noreply.github.com>
2022-09-26 18:43:32 +0530

    Assignment 5
    Octant Ranking and Naming

---
+++ tut05/tut05.py
@@ -0,0 +1,20 @@
+#Help https://youtu.be/N6PBd4XdnEw
+def octant_range_names(mod=5000):
+
+    
+    octant_name_id_mapping = {"1":"Internal outward interaction", "-1":"External outward interaction", "2":"External Ejection", "-2":"Internal Ejection", "3":"External inward interaction", "-3":"Internal inward interaction", "4":"Internal sweep", "-4":"External sweep"}
+
+###Code
+
+from platform import python_version
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+
+mod=5000 
+octant_range_names(mod)
+
